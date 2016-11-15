dde0861f242e08f917b4ede50e8e213c3e094a57
Gnana sekar <gnanasekar@Gnanas-MacBook-Pro.local>
2016-11-15 22:16:07 +0530

    UI added

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ mongoose.connection.on('connected', function(){
 
 });
 
-// CONTACTS API ROUTES BELOW
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -40,33 +39,10 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
 
 app.get("/GetFolders", function(req, res) {
     var someObj={ name:"name"};
     res.json(someObj);
 });
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-//var db;
 
-// Connect to the database before starting the application server.
-//mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-//  if (err) {
-//    console.log(err);
-//    process.exit(1);
-//  }
-//
-//  // Save database object from the callback for reuse.
-//  db = database;
-//  console.log("Database connection ready");
-//
-//  // Initialize the app.
-//  var server = app.listen(process.env.PORT || 8080, function () {
-//    var port = server.address().port;
-//    console.log("App now running on port", port);
-//  });
-//});
